54dc68e7c5a31de1ac807c02a2709336cb8ee034
Frederik Lölhöffel <6864947+floelhoeffel@users.noreply.github.com>
2022-01-21 16:06:44 +0100

    Added explicit relationship

---
+++ prisma/migrations/20220121150431_add_explicit_relationship/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "CategoriesOnPosts" (
+    "postId" INTEGER NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    "assignedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "CategoriesOnPosts_pkey" PRIMARY KEY ("postId","categoryId")
+);
+
+-- AddForeignKey
+ALTER TABLE "CategoriesOnPosts" ADD CONSTRAINT "CategoriesOnPosts_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CategoriesOnPosts" ADD CONSTRAINT "CategoriesOnPosts_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
